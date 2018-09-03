c1ca1226cdd11db5463c6e993f1c1841a59d5d90
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-03 16:20:35 +0300

    Add state object to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  state = {
+    total: null,
+    currentNum: 0
+  }
+
   render() {
     return (
       <div className="App">

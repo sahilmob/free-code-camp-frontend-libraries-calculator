3bb15933b2ccafd9ea032b567a1473ac463da9d8
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-03 17:10:02 +0300

    Add click handler methods for all buttons

--- src/App.js
+++ src/App.js
@@ -9,28 +9,45 @@ class App extends Component {
     currentNum: 0
   }
 
+  numClickHandler = (e) => {
+    console.log(e.target.innerText);
+  }
+  acClickHandler = (e) => {
+    console.log(e.target.innerText);
+  }
+  mathOpClickHandler = (e) => {
+    console.log(e.target.innerText);
+  }
+  decimalClickHandler = (e) => {
+    console.log(e.target.innerText);
+  }
+  equalsClickHandler = (e) => {
+    console.log(e.target.innerText);
+  }
+
+
   render() {
     return (
       <div className="App">
         <div></div>
         <div id="display">0</div>
-        <ButtonWide numString="clear" number="AC" />
-        <ButtonNormal numString="add" number="+" />
-        <ButtonNormal numString="subtract" number="-" />
-        <ButtonNormal numString="multiply" number="*" />
-        <ButtonNormal numString="divide" number="/" />
-        <ButtonNormal numString="one" number="1" />
-        <ButtonNormal numString="two" number="2" />
-        <ButtonNormal numString="three" number="3" />
-        <ButtonNormal numString="four" number="4" />
-        <ButtonNormal numString="five" number="5" />
-        <ButtonNormal numString="six" number="6" />
-        <ButtonNormal numString="seven" number="7" />
-        <ButtonNormal numString="eight" number="8" />
-        <ButtonNormal numString="nine" number="9" />
-        <ButtonWide numString="zero" number="0" />
-        <ButtonNormal numString="decimal" number="." />
-        <ButtonTall numString="equals" number="=" />
+        <ButtonWide numString="clear" number="AC" clicked={this.acClickHandler} />
+        <ButtonNormal numString="add" number="+" clicked={this.mathOpClickHandler} />
+        <ButtonNormal numString="subtract" number="-" clicked={this.mathOpClickHandler} />
+        <ButtonNormal numString="multiply" number="*" clicked={this.mathOpClickHandler} />
+        <ButtonNormal numString="divide" number="/" clicked={this.mathOpClickHandler} />
+        <ButtonNormal numString="one" number="1" clicked={this.numClickHandler} />
+        <ButtonNormal numString="two" number="2" clicked={this.numClickHandler} />
+        <ButtonNormal numString="three" number="3" clicked={this.numClickHandler} />
+        <ButtonNormal numString="four" number="4" clicked={this.numClickHandler} />
+        <ButtonNormal numString="five" number="5" clicked={this.numClickHandler} />
+        <ButtonNormal numString="six" number="6" clicked={this.numClickHandler} />
+        <ButtonNormal numString="seven" number="7" clicked={this.numClickHandler} />
+        <ButtonNormal numString="eight" number="8" clicked={this.numClickHandler} />
+        <ButtonNormal numString="nine" number="9" clicked={this.numClickHandler} />
+        <ButtonWide numString="zero" number="0" clicked={this.numClickHandler} />
+        <ButtonNormal numString="decimal" number="." clicked={this.decimalClickHandler} />
+        <ButtonTall numString="equals" number="=" clicked={this.equalsClickHandler} />
       </div>
     );
   }
--- src/components/ButtonTall.jsx
+++ src/components/ButtonTall.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default (props) => {
     return (
-        <button id={props.numString}>{props.number}</button>
+        <button id={props.numString} onClick={props.clicked}>{props.number}</button>
     )
 }
